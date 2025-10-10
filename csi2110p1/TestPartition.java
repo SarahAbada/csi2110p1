2fcf65abe08ef998e22c45f6e2b6b1080aa7bd82
Sarah Abada <sabad030@uottawa.ca>
2025-10-10 11:03:13 -0400

    a bunch of .class files why not commit them

---
+++ csi2110p1/TestPartition.java
@@ -0,0 +1,48 @@
+package csi2110p1;
+
+public class TestPartition {
+    public static void main(String[] args) {
+        Partition<Integer> partition = new Partition<>();
+
+        // Make singleton clusters
+        Position<Integer> a = partition.makeCluster(10);
+        Position<Integer> b = partition.makeCluster(20);
+        Position<Integer> c = partition.makeCluster(30);
+        Position<Integer> d = partition.makeCluster(40);
+
+        System.out.println("Element at position a: " + partition.element(a));
+        System.out.println("Leader of a: " + partition.find(a).element);
+        System.out.println("Leader of b: " + partition.find(b).element);
+        System.out.println("Number of clusters: " + partition.numberOfClusters());
+
+        partition.union(a, b);
+        System.out.println("\nAfter union(a, b):");
+        System.out.println("Leader of a: " + partition.find(a).element);
+        System.out.println("Leader of b: " + partition.find(b).element);
+        System.out.println("Number of clusters: " + partition.numberOfClusters());
+
+        partition.union(b, c);
+        System.out.println("\nAfter union(b, c):");
+        System.out.println("Leader of c: " + partition.find(c).element);
+        System.out.println("Cluster size for a: " + partition.clusterSize(a));
+
+        System.out.print("Positions in a's cluster: ");
+        for (Position<Integer> pos : partition.clusterPositions(a)) {
+            System.out.print(partition.element(pos) + " ");
+        }
+        System.out.println();
+
+        partition.union(d, a);
+        System.out.println("\nAfter union(d, a):");
+        System.out.println("Leader of d: " + partition.find(d).element);
+        System.out.println("Number of clusters: " + partition.numberOfClusters());
+
+        System.out.println("Cluster sizes: " + partition.clusterSizes());
+
+        System.out.print("Positions in d's cluster: ");
+        for (Position<Integer> pos : partition.clusterPositions(d)) {
+            System.out.print(partition.element(pos) + " ");
+        }
+        System.out.println();
+    }
+}
